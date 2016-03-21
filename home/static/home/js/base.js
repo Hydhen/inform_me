78d436cc04f3e5b62a2e2d25fdce57907af1cb2f
Juillet Loïc <juillet.loic@gmail.com>
2016-03-21 15:50:10 +0100

    add font-awesome add social links add menus

--- home/static/home/js/base.js
+++ home/static/home/js/base.js
@@ -1,22 +1,21 @@
-angular
-    .module('InformMe', ['ngMaterial'])
+var app = angular.module('InformMe', ['ngMaterial']);
 
-    .config(function($interpolateProvider, $mdThemingProvider) {
-        $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-        $mdThemingProvider.theme('default')
-            .primaryPalette('blue')
-            .accentPalette('grey');
-    })
+app.config(function($interpolateProvider, $mdThemingProvider) {
+    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+        .accentPalette('grey');
+})
 
-    .controller('MenuCtrl', function($scope, $mdSidenav) {
-        function toggleMenu() {
-            $mdSidenav('menu').toggle();
-        }
+app.controller('MenuCtrl', function($scope, $mdSidenav) {
+    function toggleMenu() {
+        $mdSidenav('menu').toggle();
+    }
 
-        function isMenuOpen() {
-            return $mdSidenav('menu').isOpen();
-        }
+    function isMenuOpen() {
+        return $mdSidenav('menu').isOpen();
+    }
 
-        $scope.toggleMenu = toggleMenu;
-        $scope.isMenuOpen = isMenuOpen;
-    });
+    $scope.toggleMenu = toggleMenu;
+    $scope.isMenuOpen = isMenuOpen;
+});
