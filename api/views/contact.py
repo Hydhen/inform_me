fcf049c8cf57b0c24a6d80aa1658b2fd5ff90465
Juillet Loïc <juillet.loic@gmail.com>
2016-05-10 17:39:51 +0200

    remove log

--- api/views/contact.py
+++ api/views/contact.py
@@ -2,7 +2,6 @@ from django.http import JsonResponse
 import json
 
 def Contact(request):
-    print 'contact..'
     if request.method == 'GET':
         file_path = '/data/development/inform_me/text/contact.json'
         with open(file_path, 'r') as file :
